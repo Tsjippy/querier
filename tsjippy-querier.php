86ce9264de73f4498e5ed32e08847bf0bb1e95eb
Tsjippy <ewaldha@hotmail.com>
2026-06-04 13:08:19 +0200

    changed: code layout

--- tsjippy-querier.php
+++ tsjippy-querier.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace TSJIPPY\QUERIER;
 
 /**
@@ -19,7 +20,7 @@ namespace TSJIPPY\QUERIER;
  *
  * @author Ewald Harmsen
  */
-if ( ! defined('ABSPATH')) {
+if (! defined('ABSPATH')) {
     exit;
 }
 
@@ -30,7 +31,7 @@ define(__NAMESPACE__ . '\PLUGIN', plugin_basename(__FILE__));
 define(__NAMESPACE__ . '\PLUGINPATH', __DIR__ . '/');
 define(__NAMESPACE__ . '\PLUGINVERSION', get_plugin_data(__FILE__, false, false)['Version']);
 define(__NAMESPACE__ . '\PLUGINSLUG', str_replace('tsjippy-', '', basename(__FILE__, ' .php')));
-define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_' .PLUGINSLUG. '_settings', []));
+define(__NAMESPACE__ . '\SETTINGS', get_option('tsjippy_' . PLUGINSLUG . '_settings', []));
 
 // run right before activation
 register_activation_hook(__FILE__, function () {
@@ -42,10 +43,8 @@ register_activation_hook(__FILE__, function () {
             'enquirer',
             'Enquirer',
             $roleSet
-       );
+        );
     }
 });
 
-// run on deactivation
-register_deactivation_hook(__FILE__, function () {
-});
+
